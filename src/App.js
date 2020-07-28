857857165e7704cc876f06f0a1f9ac91080b5cab
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2020-07-28 19:33:24 -0300

    charts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from 'react';
+import Chart from 'react-google-charts';
+import './style.css';
 function App() {
+  const title = 'Quantidade de cadastros do primeiro semestre';
+  const animation = {duration: 1000, asing: 'out', startup: true};
+  const [dadosChart, setDadosChart] = useState([
+    ['Mês', 'Quantidade'],
+    ['Janeiro', 30],
+    ['Fereveiro', 12],
+    ['Março', 85],
+    ['Abril', 40],
+    ['Maio', 18],
+  ]);
+
+  useEffect(() =>{
+    function alterarDados(){
+      const novosDados = dadosChart.map(line => {
+        // Pega os dados da linha na Posição 1
+        if(Number.isInteger(line[1])){
+          // Gera um número aleatório * 100.
+          line[1] = Math.floor(Math.random() * 101);
+        }
+        return line;
+      });
+      setDadosChart(novosDados); 
+    }
+    // Faz o Grafico ser atualizado a cada 5 segundos
+    const intervalId = setInterval(() => alterarDados(), 5000);
+    
+    // Limpa a atualização para não gerar um Loop Infinito
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, [dadosChart]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Dashboard</h1>
+      <div className="graphic">
+        <div>
+          <Chart 
+            width={'400px'}
+            height={'300px'}
+            chartType={'PieChart'}
+            data={dadosChart}
+            options={{title: title}}
+          />
+          <Chart 
+            width={'400px'}
+            height={'300px'}
+            chartType={'PieChart'}
+            data={dadosChart}
+            options={{
+              title: title,
+              is3D: true
+            }}
+          />   
+          <Chart 
+            width={'400px'}
+            height={'300px'}
+            chartType={'PieChart'}
+            data={dadosChart}
+            options={{
+              title: title,
+              pieHole: 0.4
+            }}
+          />
+        </div>
+        <div>
+          <Chart 
+            width={'400px'}
+            height={'300px'}
+            chartType={'BarChart'}
+            data={dadosChart}
+            options={{
+              title: title,
+              chartArea: {width: '50%'},
+              hAxis: { title: 'quantidade'},
+              vAxis: { title: 'Mês'},
+              animation: animation,
+            }}
+          />
+          <Chart 
+            width={'400px'}
+            height={'300px'}
+            chartType={'LineChart'}
+            data={dadosChart}
+            options={{
+              title: title,
+              chartArea: {width: '50%'},
+              hAxis: { title: 'Mês'},
+              vAxis: { title: 'quantidade'},
+              animation: animation,
+            }}
+          />
+          <Chart 
+            width={'400px'}
+            height={'300px'}
+            chartType={'AreaChart'}
+            data={dadosChart}
+            options={{
+              title: title,
+              chartArea: {width: '50%'},
+              hAxis: { title: 'Mês'},
+              vAxis: { title: 'quantidade'},
+              animation: animation,
+            }}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
